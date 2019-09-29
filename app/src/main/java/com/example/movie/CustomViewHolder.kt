3e95ca6f59364994853e4c77268d145ce33f4975
igor.alves <igor@novaxs.com.br>
2019-09-29 15:14:21 -0300

    removendo arquivos desnecessários

--- app/src/main/java/com/example/movie/CustomViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.example.movie
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-
-class CustomViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
-
-}
